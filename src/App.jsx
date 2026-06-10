dd12121e8002a328bba1f6147a8a5c77f3d705aa
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-06-10 10:53:02 -0500

    App.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
+import TasksMenu from "./pages/TasksMenu";
 import TaskDetails from "./pages/TaskDetails";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -30,6 +31,11 @@ export default function App() {
           <Route 
             path="/tasks"
             element={<Tasks />}
+          />
+
+          <Route 
+            path="/tasksMenu"
+            element={<TasksMenu />}
           />
 
           <Route 
