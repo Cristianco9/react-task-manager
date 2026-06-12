6f265b2c3a780c6d4bfdaef2f09477f7adf7dcd6
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-06-12 10:59:46 -0500

    Dataset ContactData created

---
+++ src/data/contactData.js
@@ -0,0 +1,8 @@
+const contactData = {
+    CompanyName: "PrimeTek Africa",
+    PhoneNumber: "+243 997 300 904",
+    mailAddress: "info@primetek.africa",
+    linkedIn: "https://www.linkedin.com/company/primetekafrica"
+};
+
+export default contactData;
